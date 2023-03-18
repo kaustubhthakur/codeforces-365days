35016dbe3a44536e7ab52253e61007b8de5ee03b
Kaustubh <kaustubhthakur76@gmail.com>
2023-03-18 19:23:51 +0530

    last commit

---
+++ codeforces/Specialist/codeforces-problems/017.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve()
+{
+    int n,k,w,d;
+    cin>>n>>k>>w>>d;
+    vector<int>a(n);
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+}
+int main()
+{  
+
+
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();    }
+
+
+
+    return 0;
+}
