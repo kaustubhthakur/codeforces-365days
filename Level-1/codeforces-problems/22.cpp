1021d6d19e432db1c85deb0f2f408bdc5d9cc9cf
Kaustubh <kaustubhthakur76@gmail.com>
2023-03-26 19:16:14 +0530

    DAY-7

--- Level-1/codeforces-problems/22.cpp
+++ Level-1/codeforces-problems/22.cpp
@@ -1,43 +1,41 @@
 #include <bits/stdc++.h>
+ 
 using namespace std;
-void solve()
-{
-    int n,m;
-    cin>>n>>m;
-    vector<int>a(n);
-  for(int i=0;i<n;i++) cin>>a[i];
-
-  set<int>st;
-  for(int i=0;i<n;i++)
-  {
-    st.insert(a[i]);
-  }
-  if((st.size()-1)==n)
-  {
-    cout<<"NO"<<endl;
-  }
-  else 
-  {
-    cout<<"YES"<<endl;
-  }
-}
+ 
+bool took[1005];
+int p[1005];
+ 
 int main()
 {
-    
-
-int t;
-cin>>t;
-while(t--)
-{
-    solve();
-}
-
-
-
-
-
-
-
-
-return 0;
+ 
+    int t; cin>>t;
+    while (t--)
+    {
+        int n; cin>>n;
+        for (int i=1;i<=n;i++) cin>>p[i];
+        for (int i=1;i<=n;i++) took[i]=0;
+        
+        if (n==1)
+        {
+            cout<<"-1\n";
+            continue;
+        }
+ 
+        for (int i=1;i<=n-2;i++)
+        {
+            int k=1;
+            while (took[k] || k==p[i]) ++k;
+            cout<<k<<" "; took[k]=1;
+        }
+ 
+        int a=-1,b=-1;
+        for (int i=1;i<=n;i++) if (!took[i])
+        {
+            if (a==-1) a=i;
+            else b=i;
+        }
+ 
+        if (a!=p[n-1] && b!=p[n]) cout<<a<<" "<<b<<"\n";
+        else cout<<b<<" "<<a<<"\n";
+    }
 }
