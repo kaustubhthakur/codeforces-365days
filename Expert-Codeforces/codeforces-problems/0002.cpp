1aaccf17199e083e19edab6a35b30f4821c23ec9
Kaustubh <kaustubhthakur76@gmail.com>
2023-03-26 20:47:38 +0530

    Day-1

---
+++ Expert-Codeforces/codeforces-problems/0002.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,m,dx,dy,dn,dm;
+        cin>>n>>m>>dx>>dy>>dm>>dn;
+    }
+    return 0;
+}
---
+++ Level-1/codeforces-problems/24.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve()
+{
+    int n,l,r;
+    cin>>n>>l>>r;
+}
+int main()
+{  
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+ 
+ 
+     }
+
+
+
+    return 0;
+}
