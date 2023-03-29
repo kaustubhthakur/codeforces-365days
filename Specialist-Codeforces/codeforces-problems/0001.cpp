09df57040de9677a051a65f6a9b2efc5ef8004c6
Kaustubh <kaustubhthakur76@gmail.com>
2023-03-29 20:34:54 +0530

    DAY-1

--- Specialist-Codeforces/codeforces-problems/0001.cpp
+++ Specialist-Codeforces/codeforces-problems/0001.cpp
@@ -3,14 +3,16 @@ using namespace std;
 int main()
 {
     int t;
-    cin>>t;
-    while(t--)
+    cin >> t;
+    while (t--)
     {
-        int n,a,b;
-        cin>>n>>a>>b;
-        string str;
-        cin>>str;
-
+        int n, a, b;
+        int ans;
+        string s;
+        cin >> n >> a >> b >> s;
+        ans = unique(s.begin(), s.end()) - s.begin();
+        cout << a * n + max(b * n, (ans / 2 + 1) * b) << endl;
     }
     return 0;
+    
 }
