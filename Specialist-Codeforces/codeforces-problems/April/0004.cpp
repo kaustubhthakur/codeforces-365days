a23beaa8ced641539e8fb823ea423bee772dd54c
Kaustubh <kaustubhthakur76@gmail.com>
2023-03-30 20:49:08 +0530

    DAY-2

---
+++ Specialist-Codeforces/codeforces-problems/April/0004.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int k, n, t, x, y;
+int main()
+{
+	
+    cin>>t;
+	while(t--)
+	{
+		cin >> n >> x >> y;
+		if(x > y)
+        swap(x, y);
+		if(x || !y || (n - 1) % y)
+		{
+			cout << -1 << endl;
+			continue;
+		}
+		for(int k = 2; k <= n; k += y)
+		{
+			for(int i = 1; i <= y; i++)cout << k << ' ';
+		}
+		cout << endl;
+	}
+    return 0;
+}
---
+++ Specialist-Codeforces/codeforces-problems/April/0006.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve()
+{
+
+
+    int n,m;
+    cin>>n>>m;
+    vector<int>a(n);
+    for(int i=0;i<n;i++)
+    cin>>a[i];
+
+    while(m--)
+    {
+        int l,r;
+        cin>>l>>r;
+    }
+
+
+
+}
+int main()
+{
+  int t;
+  cin>>t;
+  while(t--)
+  {
+    solve();
+  }
+
+      
+}
