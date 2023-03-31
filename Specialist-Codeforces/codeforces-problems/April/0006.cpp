1f21c956f9fbdabbc5e3eb29a85bbf3db5c4b87a
Kaustubh <kaustubhthakur76@gmail.com>
2023-03-31 19:33:40 +0530

    DAY-3

--- Specialist-Codeforces/codeforces-problems/April/0006.cpp
+++ Specialist-Codeforces/codeforces-problems/April/0006.cpp
@@ -1,22 +1,27 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-void solve()
-{
-
-
-    
-
-
-
-}
+int t, a, b;
 int main()
 {
-  int t;
-  cin>>t;
-  while(t--)
+  cin >> t;
+  while (t--)
   {
-    solve();
+    cin >> a >> b;
+    int ans = 0, sol = 0;
+    for (int i = a; i <= min(b, a + 100); i++)
+    {
+      int x = i, maxi = 0, mini = 10;
+      while (x > 0)
+      {
+        maxi = max(x % 10, maxi);
+        mini = min(x % 10, mini);
+        x /= 10;
+      }
+      if (ans <= maxi - mini)
+        ans = maxi - mini,
+        sol = i;
+    }
+    cout << sol << endl;
   }
-
-      
+  return 0;
 }
