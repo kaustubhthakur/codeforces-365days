b7233505e290b74846a9b53275934e6f9948659e
Kaustubh <kaustubhthakur76@gmail.com>
2023-04-03 22:26:11 +0530

    DAY-3

--- Specialist-Codeforces/codeforces-problems/April/0015.cpp
+++ Specialist-Codeforces/codeforces-problems/April/0015.cpp
@@ -2,16 +2,7 @@
 using namespace std;
 void solve()
 {
-    int n,m;
-    cin>>n>>m;
-    int grid[i][j];
-    for(int i=0;i<n;i++)
-    {
-        for(int j=0;j<m;j++)
-        {
-            cin>>grid[i][j];
-        }
-    }
+ 
 }
 int main()
 {
