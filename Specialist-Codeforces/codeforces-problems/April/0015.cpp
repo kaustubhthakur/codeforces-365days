b40b622c1d413a768d1f3d263ee50de3547932dd
Kaustubh <kaustubhthakur76@gmail.com>
2023-04-05 19:54:22 +0530

    DAY-3

--- Specialist-Codeforces/codeforces-problems/April/0015.cpp
+++ Specialist-Codeforces/codeforces-problems/April/0015.cpp
@@ -2,13 +2,24 @@
 using namespace std;
 void solve()
 {
- 
+    int x, y;
+    cin >> x >> y;
+    int d = x * x + y * y;
+    int l = 0;
+    while (l * l < d)
+        ++l;
+    int ans = 2;
+    if (l * l == d)
+        ans = 1;
+    if (x == 0 && y == 0)
+        ans = 0;
+    cout << ans << endl;
 }
 int main()
 {
     int t;
-    cin>>t;
-    while(t--)
+    cin >> t;
+    while (t--)
     {
         solve();
     }
