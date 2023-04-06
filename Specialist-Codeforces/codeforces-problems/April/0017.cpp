8f819cff6d4dbeed2c2dd7906ca6e4f7639f9c13
Kaustubh <kaustubhthakur76@gmail.com>
2023-04-06 19:13:59 +0530

    DAY-4

--- Specialist-Codeforces/codeforces-problems/April/0017.cpp
+++ Specialist-Codeforces/codeforces-problems/April/0017.cpp
@@ -6,27 +6,31 @@ void solve()
 {
 
     int n, x;
-cin>>n>>x;
-		for (int i = 1; i <= n; i++) {
-			cin>>numbers[i];
-		}
-		int num_min = numbers[1];
-		int num_max = numbers[1];
-		int res = 0;
-		for (int i = 2; i <= n; i++) {
-			if (numbers[i] > num_max) {
-				num_max = numbers[i];
-			}
-			if (numbers[i] < num_min) {
-				num_min = numbers[i];
-			}
-			if (num_max - num_min > 2 * x) {
-				res++;
-				num_min = num_max = numbers[i];
-			}
-		}
-	cout<<res<<endl;
-    
+    cin >> n >> x;
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> numbers[i];
+    }
+    int num_min = numbers[1];
+    int num_max = numbers[1];
+    int res = 0;
+    for (int i = 2; i <= n; i++)
+    {
+        if (numbers[i] > num_max)
+        {
+            num_max = numbers[i];
+        }
+        if (numbers[i] < num_min)
+        {
+            num_min = numbers[i];
+        }
+        if (num_max - num_min > 2 * x)
+        {
+            res++;
+            num_min = num_max = numbers[i];
+        }
+    }
+    cout << res << endl;
 }
 int main()
 {
--- Specialist-Codeforces/codeforces-problems/April/0018.cpp
+++ Specialist-Codeforces/codeforces-problems/April/0018.cpp
@@ -1,7 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
+void solve()
+{
+    int n;
+    cin>>n;
+    vector<int>a(n);
+    for(int i=0;i<n;i++)
+    cin>>a[i];
+}
 int main()
 {
 
+int t;
+cin>>t;
+while(t--)
+{
+    solve();
+
+}
     return 0;
 }
