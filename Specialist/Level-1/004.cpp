b54cba803abcb396431fab6ed1fb8c8ac1a5c881
Kaustubh <kaustubhthakur76@gmail.com>
2023-04-10 20:45:58 +0530

    Day-2

---
+++ Specialist/Level-1/004.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        long long a, b;
+        cin >> a >> b;
+        cout << 2 << endl;
+        cout << a - 1 << " " << 1 << endl;
+        cout << a << " " << b << endl;
+    }
+    return 0;
+}
