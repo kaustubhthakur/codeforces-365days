2d151053532437ed89d5c9ff3246375be0e6d653
Kaustubh <kaustubhthakur76@gmail.com>
2023-04-17 20:23:03 +0530

    initial commit

---
+++ problems/section-1/14.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int x;
+        for(int i=0;i<n;i++)
+        {
+            cin>>x;
+
+        }
+        while(n--)
+        {
+            int dx;
+            string str;
+            cin>>dx>>str;
+        }
+    }
+}
