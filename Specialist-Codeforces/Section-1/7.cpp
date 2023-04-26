ee999cd75daad546d320aa4f9187b00ab6603823
Kaustubh <kaustubhthakur76@gmail.com>
2023-04-26 20:39:19 +0530

    initial commit

--- Specialist-Codeforces/Section-1/7.cpp
+++ Specialist-Codeforces/Section-1/7.cpp
@@ -1,12 +1,23 @@
-#include <bits/stdc++.h>
+
+#include<bits/stdc++.h>
+
 using namespace std;
+
 int main()
 {
-    int t;
-    cin>>t;
-    while(t--)
-    {
-        
-    }
+	int t;
+	cin >> t;
+	for(int i = 0; i < t; i++)
+	{
+		string s;
+		cin >> s;
+		int ans = 1;
+		if(s[0] == '0') ans = 0;
+		if(s[0] == '?') ans = 9;
+		for(int j = 1; j < s.size(); j++)
+			if(s[j] == '?')
+				ans *= 10;
+		cout << ans << endl;
+	}
     return 0;
 }
