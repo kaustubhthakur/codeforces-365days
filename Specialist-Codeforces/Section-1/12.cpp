3258206f8286dce7bf9f6d2d9acc10bbd67f34b0
Kaustubh <kaustubhthakur76@gmail.com>
2023-05-01 23:34:00 +0530

    DAY-1

--- Specialist-Codeforces/Section-1/12.cpp
+++ Specialist-Codeforces/Section-1/12.cpp
@@ -1,28 +1,22 @@
 #include <bits/stdc++.h>
-using namespace std;
+int t, n, a, b, c, d, i;
+
 int main()
 {
-    int t;
-    cin>>t;
-    while(t--)
+    scanf("%d", &t);
+    for (; t--; a = b = c = d = i = 0)
     {
-        long long n;
-        cin>>n;
-        long long Awhite=1,Ablack=0,Bwhite=0,Bblack=0;
-     for(int i=2;i<=n;i+=i)
-     {
-        for(int j=1;i<=2;j++)
-        {
-            if(i%2==0)
-            {
-                Bblack++;
-            }
-            else 
-            {
-                Bwhite++;
-            }
-        }
-     }
+        scanf("%d", &n);
+        for (; ++i;)
+            if (n < i)
+                if (i % 4 < 2)
+                    a += n - n / 2, b += n / 2, i = -1;
+                else
+                    c += n / 2, d += n - n / 2, i = -1;
+            else if (i % 4 < 2)
+                a += i - i / 2, b += i / 2, n -= i;
+            else
+                c += i / 2, d += i - i / 2, n -= i;
+        printf("%d %d %d %d\n", a, b, c, d);
     }
-    return 0;
 }
