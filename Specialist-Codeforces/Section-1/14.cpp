7474df7be14ad0d20bce07ab0278065afbf12341
Kaustubh <kaustubhthakur76@gmail.com>
2023-05-02 20:35:31 +0530

    DAY-3

---
+++ Specialist-Codeforces/Section-1/14.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        string str;
+        cin>>str;
+        for(int i=0;i<n;i++)
+        {
+            if(str[i]=='U')
+            {
+                str[i] = 'D';
+              
+            }
+            else if(str[i]=='D')
+            {
+                str[i] = 'U';
+                
+            }
+        }
+        cout<<str<<" ";
+        cout<<endl;
+    }
+
+}
