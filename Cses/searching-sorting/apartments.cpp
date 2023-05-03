86c7356615b4850ec6c15668bbb515b68dcaf2b2
Kaustubh <kaustubhthakur76@gmail.com>
2023-05-04 00:33:57 +0530

    DAY-2

--- Cses/searching-sorting/apartments.cpp
+++ Cses/searching-sorting/apartments.cpp
@@ -5,25 +5,25 @@ int main()
     int n, m, k;
     cin >> n >> m >> k;
     vector<int> a(n);
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; ++i)
     {
         cin >> a[i];
     }
     vector<int> b(m);
-    for (int i = 0; i < m; i++)
+    for (int i = 0; i < m; ++i)
     {
         cin >> b[i];
     }
     int ans = 0;
-    for (int i = 0; i < n; i++)
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+    for (int i =0,j= 0; i < n; ++i)
     {
-        for (int j = 0; j < m; j++)
-        {
-            if ( a[i] == b[j] || a[i] - k == b[j] || a[i] + k == b[j])
-            {
-                ans++;
-            }
-        }
+        while (j < m && b[j] < (a[i] - k))
+           ++j;
+        if (j < m && b[j] <= (a[i] + k))
+            ++ans,++j;
+        
     }
     cout << ans << endl;
     return 0;
